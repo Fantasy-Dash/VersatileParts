98956c63920af0816b59204988adeb3d4486a98a
Fantasy-Dash <74521319+Fantasy-Dash@users.noreply.github.com>
2023-03-24 14:49:51 +0800

    增加通用的 任务/触发器等待器

---
+++ src/VP.Quartz/Listeners/WaitTriggerListener.cs
@@ -0,0 +1,31 @@
+﻿using Quartz;
+using Quartz.Listener;
+
+namespace VP.Quartz.Listeners
+{
+    public class WaitTriggerListener : TriggerListenerSupport, IDisposable
+    {
+        private readonly ManualResetEvent manualResetEvent = new(false);
+
+        private readonly string name = Guid.NewGuid().ToString();
+
+        public override string Name { get => name; }
+
+        public override Task TriggerComplete(ITrigger trigger, IJobExecutionContext context, SchedulerInstruction triggerInstructionCode, CancellationToken cancellationToken = default)
+        {
+            manualResetEvent.Set();
+            return Task.CompletedTask;
+        }
+
+        public void WaitForJobToComplete()
+        {
+            manualResetEvent.WaitOne();
+        }
+
+        public void Dispose()
+        {
+            manualResetEvent.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
