bf21f356317f99cc46aede4fce575116b53ee944
Fantasy-Dash <74521319+Fantasy-Dash@users.noreply.github.com>
2023-04-11 11:14:52 +0800

    完善excel扩展方法

---
+++ src/VP.Office/Extensions/IRowExtensions.cs
@@ -0,0 +1,20 @@
+﻿using NPOI.SS.UserModel;
+using System.Drawing;
+using VP.Office.Helpers;
+
+namespace VP.Office.Extensions
+{//todo 注释
+    public static class ISheetExtensions
+    {
+
+        public static ICell? GetCellByPoint(this ISheet sheet, Point point) => sheet.GetRow(point.X)?.GetCell(point.Y);
+
+        public static IEnumerable<ICell?> GetColumnCells(this ISheet sheet, int columnIndex, int startIndex = 0)
+        {
+            var ret = new List<ICell?>();
+            for (int i = startIndex; i <= sheet.LastRowNum; i++)
+                ret.Add(sheet.GetRow(i)?.GetCell(columnIndex));
+            return ret;
+        }
+    }
+}
