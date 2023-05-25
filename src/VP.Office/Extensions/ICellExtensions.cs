e5aeae914cc346e9570cf77c7678bdf9932082d8
Fantasy-Dash <74521319+Fantasy-Dash@users.noreply.github.com>
2023-05-25 16:23:23 +0800

    增加获取单元格字符串扩展方法

---
+++ src/VP.Office/Extensions/ICellExtensions.cs
@@ -0,0 +1,22 @@
+﻿using NPOI.SS.UserModel;
+
+namespace VP.Office.Extensions
+{
+    //todo
+    public static class ExcelICellExtensions
+    {
+        public static string? GetStringValue(this ICell? cell)
+        {
+            if(cell is null) return null;
+            return cell.CellType switch
+            {
+                CellType.Unknown => cell.ToString(),
+                CellType.Numeric => cell.NumericCellValue.ToString(),
+                CellType.String or CellType.Formula => cell.StringCellValue,
+                CellType.Blank => string.Empty,
+                CellType.Boolean => cell.BooleanCellValue.ToString(),
+                _ => null,
+            };
+        }
+    }
+}
