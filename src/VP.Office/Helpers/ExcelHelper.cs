05ed8ce6675819438b973ca2693374d88f87afd2
Fantasy-Dash <74521319+Fantasy-Dash@users.noreply.github.com>
2023-05-31 16:20:09 +0800

    字母数字转换方法更改为调用npoi自带的CellReference类中的方法

--- src/VP.Office/Helpers/ExcelHelper.cs
+++ src/VP.Office/Helpers/ExcelHelper.cs
@@ -1,9 +1,11 @@
 ﻿using NPOI.HSSF.UserModel;
 using NPOI.POIFS.FileSystem;
 using NPOI.SS.UserModel;
+using NPOI.SS.Util;
 using NPOI.XSSF.Streaming;
 using NPOI.XSSF.UserModel;
 using System.Text;
+using VP.Common.Helpers;
 
 namespace VP.Office.Helpers
 {//todo 注释
@@ -11,6 +13,8 @@ namespace VP.Office.Helpers
     {
         public static IEnumerable<ISheet> ReadFileToSheetList(string filePath)
         {
+            if(FileHelper.IsFileUsing(filePath))
+                throw new FileLoadException("文件已被占用");
             var ret = new List<ISheet>();
             try
             {
@@ -72,21 +76,7 @@ namespace VP.Office.Helpers
         /// </summary>
         /// <param name="letters">要转换的字母串</param>
         /// <returns>转换后的数字</returns>
-        public static int GetColumnIndexFromColumnLetters(string? letters)
-        {
-            if (string.IsNullOrWhiteSpace(letters))
-                throw new ArgumentException(null, nameof(letters));
-            int result = 0;
-            int power = 1;
-            for (int i = letters.Length - 1; i >= 0; i--)//倒序遍历
-            {
-                char letter = letters[i];
-                int value = letter - 'A' + 1; // 将字母转换为数字（A对应1，B对应2，以此类推）
-                result += value * power;
-                power *= 26;
-            }
-            return result - 1;
-        }
+        public static int GetColumnIndexFromColumnLetters(string? letters) => CellReference.ConvertColStringToIndex(letters);
 
         /// <summary>
         /// 将数字转换为字母串
@@ -94,17 +84,6 @@ namespace VP.Office.Helpers
         /// <param name="index">要转换的数字</param>
         /// <returns>转换后的字母串</returns>
         /// <exception cref="ArgumentOutOfRangeException"><paramref name="index"/>不能为负数</exception>
-        public static string GetColumnLettersFromColumnIndex(int index)
-        {
-            if (index<0) throw new ArgumentOutOfRangeException(nameof(index));
-            var result = new StringBuilder();
-            while (index/26>0)
-            {
-                _=result.Append((char)((index/26)+'A'-1));
-                index%=26;
-            }
-            _=result.Append((char)(index+'A'));
-            return result.ToString();
-        }
+        public static string GetColumnLettersFromColumnIndex(int index) => CellReference.ConvertNumToColString(index);
     }
 }
