354c18fedc271cc2c654dfc297621f8f4a9b04f6
Fantasy-Dash <74521319+Fantasy-Dash@users.noreply.github.com>
2023-06-02 11:11:30 +0800

    office 增加创建或获取子级别元素的扩展方法

---
+++ src/VP.Office/Extensions/IRowExtensions.cs
@@ -0,0 +1,17 @@
+﻿using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VP.Office.Extensions
+{
+    public static class IRowExtensions
+    {
+        public static ICell GetOrCreateCell(this IRow row,int colIndex)
+        {
+            return row.GetCell(colIndex)??row.CreateCell(colIndex);
+        }
+    }
+}
